d8ee3fb0bcb11b579ecb1511aaed0ad6b397906c
Eakteker jahan Liza <eaktekerjahan@gmail.com>
2024-10-07 21:28:44 +0600

    update projectSecurityConfig

--- src/main/java/com/example/easybank/config/projectSecurityConfig.java
+++ src/main/java/com/example/easybank/config/projectSecurityConfig.java
@@ -1,21 +1,20 @@
 package com.example.easybank.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class projectSecurityConfig {
 
-   @Bean
-   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((requests)-> requests.anyRequest().permitAll());
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
-        return  http.build();
+        return http.build();
     }
 }
