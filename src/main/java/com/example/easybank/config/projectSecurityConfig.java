c33347803df022cf488c54d636ebe42d38089216
Eakteker jahan Liza <eaktekerjahan@gmail.com>
2024-10-08 06:41:42 +0600

    create bean PasswordEncoder and UserDetailsService

--- src/main/java/com/example/easybank/config/projectSecurityConfig.java
+++ src/main/java/com/example/easybank/config/projectSecurityConfig.java
@@ -3,8 +3,14 @@ package com.example.easybank.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -12,9 +18,37 @@ public class projectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+       // 1/ http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+       // 1/http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/my-account","/my-balance").authenticated()
+                        .requestMatchers("/notice","/error").permitAll());
+           http.formLogin(withDefaults());
+         http.httpBasic(withDefaults());
+
+         //2/ http.formLogin(AbstractHttpConfigurer::disable);
+        //2/http.httpBasic(fcl-> fcl.disable());
+
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+      UserDetails user=  User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        UserDetails admin=  User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+      return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+      return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
